0eafa2ae48b675b8237fc847c609b3f2bd479a5e
Mati-Bowery <matias@bowerystudio.co>
2022-02-12 19:58:26 +0000

    Contact Form

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,6 @@ module.exports = {
     title: "on time creative",
   },
   plugins: [
-    //form with Netlify forms
-    "gatsby-plugin-netlify",
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",

ad1426caf764bededbd4af3b393dd8d588f6cf3f
MatiCanzani <matiascanzani@gmail.com>
2025-07-17 08:35:30 +0100

    Add facebook tags server side

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -26,34 +26,6 @@ const Layout = ({ children }) => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1258282558902220');
-            fbq('track', 'PageView');
-          `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1258282558902220&ev=PageView&noscript=1"
-            alt="fb-pixel"
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
       </Helmet>
 
       <div className="background">
